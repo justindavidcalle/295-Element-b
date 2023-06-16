afa83c04609ff09d45c3be1f0385a074087ea7c6
justindavidcalle <justincalle@outlook.de>
2023-06-16 10:26:24 +0200

    GET/{id} erstellt
    Beim Endpunkt GET/{id} kommt die spezifische Task mit dieser ID.

--- todo.js
+++ todo.js
@@ -8,6 +8,7 @@ app.use(express.urlencoded({extended: true}));
 
 const tasklist = [
     { 
+        id: 0,
         title: 'todo1',
         createdDate:"16.6.2023",
         executionDate:"13.2" 
@@ -15,7 +16,14 @@ const tasklist = [
 ];
 
 app.get(`/tasks`, (request, response) =>{
-    response.status(200).send(tasklist + new Date())
+    response.status(200).json(tasklist)
+})
+
+app.post(`/tasks`, (request, response) =>{
+    const newTask = request.body;
+    tasklist.push(newTask);
+    const newTaskId = tasklist.lastIndexOf(tasklist)
+    response.status(201).json(tasklist).send(newTaskId);
 })
 
 app.listen(port, ()=>{
